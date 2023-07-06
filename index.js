f1bef69b09642d9bb26f545b46389064583c9aea
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2023-07-06 06:41:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const yargs = require('yargs')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+	command: 'add',
+	describe: 'Adds two number',
+	builder: {
+		firstNumber: {
+			describe: 'First Number',
+			demandOption: true, // Required
+			type: 'number'	
+		},
+		secondNumber: {
+			describe: 'Second Number',
+			demandOption: true,
+			type: 'number'
+		}
+	},
+
+	// Function for your command
+	handler(argv) {
+		console.log("Result:",
+			(argv.firstNumber+argv.secondNumber))
+	}
+})
+
+yargs.parse() // To set above changes
